28a1df742c1ebfdb866b55d4f3b73b87d8757246
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2019-04-19 13:34:07 +0530

    dialog box ui fixes

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -5,9 +5,13 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -19,32 +23,50 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         applyButton=(Button)findViewById(R.id.apply_button);
 
+//        applyButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(
+//                        SettingsActivity.this);
+//                builder.setTitle("Save Settings?");
+//                builder.setMessage("Do you want to save your answers and enable one touch apply on Internshala widget?");
+//                builder.setNegativeButton("NO",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog,
+//                                                int which) {
+//                                Toast.makeText(getApplicationContext(),"No is clicked",Toast.LENGTH_LONG).show();
+//                            }
+//                        });
+//                builder.setPositiveButton("YES",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog,
+//                                                int which) {
+//                                Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
+//                            }
+//                        });
+//                builder.show();
+//            }
+//        });
+
         applyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(
-                        SettingsActivity.this);
-                builder.setTitle("Save Settings?");
-                builder.setMessage("Do you want to save your answers and enable one touch apply on Internshala widget?");
-                builder.setNegativeButton("NO",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-                                Toast.makeText(getApplicationContext(),"No is clicked",Toast.LENGTH_LONG).show();
-                            }
-                        });
-                builder.setPositiveButton("YES",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-                                Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
-                            }
-                        });
-                builder.show();
+
+
+                LayoutInflater inflater = getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.layout_custom_dialog, null);
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(SettingsActivity.this);
+                // this is set the view from XML inside AlertDialog
+                alert.setView(alertLayout);
+                // disallow cancel of AlertDialog on click of back button and outside touch
+                AlertDialog dialog = alert.create();
+                dialog.show();
             }
         });
 
-    }
+        }
+
 
     @Override
     public void onBackPressed() {
